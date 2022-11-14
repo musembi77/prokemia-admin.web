55fad1928454188fe26806d976d4475ee50ed0d9
dennissammy77 <dennissammy77@gmail.com>
2022-11-14 20:26:39 +0300

    New features

--- components/Product.js
+++ components/Product.js
@@ -6,7 +6,7 @@ function Product(){
 	const router = useRouter()
 	return(
 		<Flex bg='#eee' borderRadius='5px' direction='column' m='2' w='170px'>
-			<Image bg='#fff' w='100%' h='50px' borderRadius='5px 5px 0px 0px' src='./Pro.png' objectFit='cover' border='1px solid #eee'/>
+			<Image bg='#fff' w='100%' h='50px' borderRadius='5px 5px 0px 0px' src='./Pro.png' objectFit='cover' border='1px solid #eee' alpt='photo'/>
 			<Flex p='2' direction='column' w='200px' gap='2'>
 				<Text color='#009393' fontWeight='bold' fontSize="24px">Cereal</Text>
 				<Flex>
@@ -19,7 +19,7 @@ function Product(){
 				</Flex>
 				<Flex gap='2'>
 					<Text fontWeight='bold'>Brand:</Text>
-					<Text>' '</Text>
+					<Text></Text>
 				</Flex>
 				<Text fontWeight='bold' p='1' cursor='pointer' onClick={(()=>{router.push(`/product/1`)})}>View product -&gt;  </Text>
 			</Flex>
