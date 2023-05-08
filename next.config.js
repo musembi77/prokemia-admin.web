ead24dc2108e3d66caa963baa3db1475471cbb17
musembi sammy <79364582+musembi77@users.noreply.github.com>
2023-05-08 08:41:56 +0300

    edited config file to remove webpack op

--- next.config.js
+++ next.config.js
@@ -2,17 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { isServer }) => {
-    
-    // If client-side, don't polyfill `fs`
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-
-    return config;
-  },
 }
 
 module.exports = nextConfig
